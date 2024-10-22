ba69596c8bc8bd4655a84034eebad54657d591ed
Moaz2025 <moaz20ahmed@gmail.com>
2024-10-22 15:41:09 +0300

    Initial backend files

---
+++ Backend/src/test/java/com/sakan/SakanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
