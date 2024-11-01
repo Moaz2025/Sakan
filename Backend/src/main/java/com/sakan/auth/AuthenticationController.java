84f1817067c35b64f635f84e4968565fa1276d20
Ziad Reda <engziadreda@gmail.com>
2024-11-01 17:23:49 +0200

    Email/Password format validation & duplicate emails validation

--- Backend/src/main/java/com/sakan/auth/AuthenticationController.java
+++ Backend/src/main/java/com/sakan/auth/AuthenticationController.java
@@ -1,6 +1,10 @@
 package com.sakan.auth;
 
+import com.sakan.user.UserRepository;
+import com.sakan.validation.EmailValidator;
+import com.sakan.validation.PasswordValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+    private final UserRepository userRepository;
+    private final EmailValidator emailValidator;
+    private final PasswordValidator passwordValidator;
 
     @PostMapping("/signUp")
-    public ResponseEntity<AuthenticationResponse> signUP(@RequestBody SignUpRequest signUpRequest) {
+    public ResponseEntity<AuthenticationResponse> signUp(@RequestBody SignUpRequest signUpRequest) {
+        if (!emailValidator.isValid(signUpRequest.getEmail()))
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new AuthenticationResponse("Invalid Email"));
+
+        if (!passwordValidator.isValid(signUpRequest.getPassword()))
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new AuthenticationResponse("Invalid Password"));
+
+        if (userRepository.findByEmail(signUpRequest.getEmail()).isPresent())
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new AuthenticationResponse("Email Already Exist"));
+
         return ResponseEntity.ok(authenticationService.signUp(signUpRequest));
     }
 
---
+++ Backend/src/main/java/com/sakan/validation/EmailValidator.java
@@ -0,0 +1,19 @@
+package com.sakan.validation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+@RequiredArgsConstructor
+public class EmailValidator implements IValidator {
+    @Override
+    public boolean isValid(String email){
+        String regex = "^[\\w!#$%&'+/=?`{|}~^-]+(?:\\.[\\w!#$%&'+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return (matcher.matches());
+    }
+}
